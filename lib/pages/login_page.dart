a4958c464ef74f046d332287dc08493f2ebb0e44
Ishan-Hegde <110191499+Ishan-Hegde@users.noreply.github.com>
2024-09-23 15:38:30 +0530

    first push
    mitchkoko(6:03)

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+
+            //message, app slogan
+            Text(
+              "Green Table",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(height: 25),
+
+            // email textfield
+            TextField(),
+            //password textfield
+
+            //sign in button
+
+            //not a member? register now
+          ],
+        ),
+      ),
+    );
+  }
+}
