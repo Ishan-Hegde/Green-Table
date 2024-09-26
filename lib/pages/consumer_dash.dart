d57d28b7a21364084ea67cb6aaed29fe8fb29cdd
Ishan-Hegde <110191499+Ishan-Hegde@users.noreply.github.com>
2024-09-26 15:06:28 +0530

    added dark mode, need to add menu drawer

--- lib/pages/consumer_dash.dart
+++ lib/pages/consumer_dash.dart
@@ -16,20 +16,12 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ConsumerApp(),
-    );
-  }
-}
-
 class _ConsumerAppState extends State<ConsumerApp> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 0; // Track the selected index for bottom navigation
+  bool isDarkMode = false; // Track whether dark mode is on or off
+
+  // Define the primary green color used for the app
+  Color primaryGreen = Colors.green[700]!;
 
   // Navigation items
   final List<Widget> _widgetOptions = <Widget>[
@@ -40,84 +32,146 @@ class _ConsumerAppState extends State<ConsumerApp> {
 
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      _selectedIndex = index; // Update the selected index
+    });
+  }
+
+  // Toggle dark mode
+  void _toggleDarkMode() {
+    setState(() {
+      isDarkMode = !isDarkMode; // Toggle dark mode
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-            decoration: BoxDecoration(
-              color: Colors.green[700], // Header background color
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(55),
-                bottomRight: Radius.circular(55),
-              ),
-            ),
-            child: SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.menu, color: Colors.white),
-                    onPressed: () {
-                      // Add logic for menu button, if needed
-                    },
-                  ),
-                  const Text(
-                    'FooD for GooD',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.notifications, color: Colors.white),
-                    onPressed: () {
-                      // Add notification logic
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          // Body content below AppBar
-          Expanded(
-            child: _widgetOptions[_selectedIndex],
-          ),
-        ],
+    return MaterialApp(
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: primaryGreen,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.green,
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant),
-            label: 'Home',
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: primaryGreen,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            // Custom AppBar
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              decoration: BoxDecoration(
+                color: isDarkMode ? Colors.black : primaryGreen,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(55),
+                  bottomRight: Radius.circular(55),
+                ),
+              ),
+              child: SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.menu, color: Colors.white),
+                      onPressed: () {
+                        // Show a modal bottom sheet or dialog for menu
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return _buildMenuOptions();
+                          },
+                        );
+                      },
+                    ),
+                    const Text(
+                      'FooD for GooD',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    IconButton(
+                      icon:
+                          const Icon(Icons.notifications, color: Colors.white),
+                      onPressed: () {
+                        // Add notification logic here
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Body content below AppBar
+            Expanded(
+              child: _widgetOptions[_selectedIndex],
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.restaurant),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_shipping),
+              label: 'Nearby',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              label: 'History',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: primaryGreen,
+          selectedFontSize: 16,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          selectedIconTheme: const IconThemeData(size: 26),
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+
+  // Menu options, including dark mode toggle
+  Widget _buildMenuOptions() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      height: 200,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            leading: Icon(Icons.brightness_6,
+                color: isDarkMode ? Colors.yellow : Colors.grey),
+            title: const Text('Dark Mode'),
+            trailing: Switch(
+              value: isDarkMode,
+              onChanged: (value) {
+                _toggleDarkMode();
+                Navigator.pop(context); // Close the menu after toggling
+              },
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_shipping),
-            label: 'Nearby',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
+          // Add more menu items if needed
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green[700],
-        selectedFontSize: 16,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-        selectedIconTheme: const IconThemeData(size: 26),
-        onTap: _onItemTapped,
       ),
     );
   }
 }
 
+// Page 1: Food For Good
 class FoodForGoodPage extends StatefulWidget {
   @override
   _FoodForGoodPageState createState() => _FoodForGoodPageState();
@@ -150,7 +204,7 @@ class _FoodForGoodPageState extends State<FoodForGoodPage> {
   void _filterSearchResults(String query) {
     if (query.isEmpty) {
       setState(() {
-        _filteredRestaurants = _restaurants;
+        _filteredRestaurants = _restaurants; // Show all when query is empty
       });
     } else {
       setState(() {
@@ -168,7 +222,7 @@ class _FoodForGoodPageState extends State<FoodForGoodPage> {
       margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
       child: Column(
         children: [
-          // Reduced padding around the search bar
+          // Search bar
           Container(
             margin: const EdgeInsets.only(bottom: 10.0),
             child: TextField(
@@ -189,14 +243,20 @@ class _FoodForGoodPageState extends State<FoodForGoodPage> {
           Expanded(
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: Theme.of(context).brightness == Brightness.dark
+                    ? Colors.grey[850] // Dark mode box color
+                    : Colors.white, // Light mode box color
                 borderRadius: BorderRadius.circular(10.0),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
+                    color: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.black
+                            .withOpacity(0.5) // Darker shadow for dark mode
+                        : Colors.black
+                            .withOpacity(0.2), // Lighter shadow for light mode
                     spreadRadius: 3,
                     blurRadius: 5,
-                    offset: const Offset(0, 3), // changes position of shadow
+                    offset: const Offset(0, 3), // Shadow position
                   ),
                 ],
               ),
@@ -210,8 +270,23 @@ class _FoodForGoodPageState extends State<FoodForGoodPage> {
                       height: 50,
                       width: 50,
                     ),
-                    title: Text(restaurant['name']!),
-                    subtitle: Text(restaurant['items']!),
+                    title: Text(
+                      restaurant['name']!,
+                      style: TextStyle(
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors
+                                .black, // Explicitly set the text color for light/dark mode
+                      ),
+                    ),
+                    subtitle: Text(
+                      restaurant['items']!,
+                      style: TextStyle(
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.grey[300]
+                            : Colors.grey[800], // Adjust subtitle color
+                      ),
+                    ),
                   );
                 },
               ),
@@ -228,47 +303,37 @@ class AvailablePickupsPage extends StatelessWidget {
   final List<Map<String, String>> availablePickups = [
     {'name': 'Taj Lands End', 'time': '2:30 PM', 'items': '10 items'},
     {'name': 'Balaji', 'time': '2:30 PM', 'items': '10 items'},
-    {'name': 'Amrut Sagar', 'time': '2:30 PM', 'items': '10 items'},
-    {'name': 'BBQ Nation', 'time': '2:30 PM', 'items': '10 items'},
-    {'name': 'Sai Palace', 'time': '2:30 PM', 'items': '10 items'},
-    {'name': 'Global Fusion', 'time': '2:30 PM', 'items': '10 items'},
+    {'name': 'Alibaug', 'time': '2:30 PM', 'items': '10 items'},
+    {'name': 'The Great Indian Dhaba', 'time': '2:30 PM', 'items': '10 items'},
+    {'name': 'McDonald’s', 'time': '2:30 PM', 'items': '10 items'},
+    {'name': 'Food Factory', 'time': '2:30 PM', 'items': '10 items'},
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3), // changes position of shadow
-          ),
-        ],
-      ),
-      child: ListView.builder(
-        itemCount: availablePickups.length,
-        itemBuilder: (context, index) {
-          final pickup = availablePickups[index];
-          return ListTile(
+    return ListView.builder(
+      itemCount: availablePickups.length,
+      itemBuilder: (context, index) {
+        final pickup = availablePickups[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          elevation: 4,
+          child: ListTile(
             title: Text(pickup['name']!),
-            subtitle: Text('${pickup['time']} - ${pickup['items']}'),
+            subtitle: Text('${pickup['time']} - ${pickup['items']} available'),
             trailing: ElevatedButton(
-              onPressed: () {
-                // Accept pickup logic
-              },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green[600],
+                backgroundColor:
+                    Colors.green[700], // Set the button color to the same green
               ),
-              child: const Text('Accept'),
+              child: const Text('Claim'),
+              onPressed: () {
+                // Add claim logic
+              },
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
@@ -276,41 +341,35 @@ class AvailablePickupsPage extends StatelessWidget {
 // Page 3: Past Orders
 class PastOrdersPage extends StatelessWidget {
   final List<Map<String, String>> pastOrders = [
-    {'name': 'Taj Lands End', 'status': 'Successful'},
-    {'name': 'Balaji', 'status': 'Successful'},
-    {'name': 'Amrut Sagar', 'status': 'Successful'},
-    {'name': 'BBQ Nation', 'status': 'Successful'},
-    {'name': 'Sai Palace', 'status': 'Successful'},
-    {'name': 'Global Fusion', 'status': 'Successful'},
+    {'name': 'Food Item 1', 'date': 'September 12, 2023'},
+    {'name': 'Food Item 2', 'date': 'September 13, 2023'},
+    {'name': 'Food Item 3', 'date': 'September 14, 2023'},
+    {'name': 'Food Item 4', 'date': 'September 15, 2023'},
+    {'name': 'Food Item 5', 'date': 'September 16, 2023'},
+    {'name': 'Food Item 6', 'date': 'September 17, 2023'},
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3), // changes position of shadow
-          ),
-        ],
-      ),
-      child: ListView.builder(
-        itemCount: pastOrders.length,
-        itemBuilder: (context, index) {
-          final order = pastOrders[index];
-          return ListTile(
+    return ListView.builder(
+      itemCount: pastOrders.length,
+      itemBuilder: (context, index) {
+        final order = pastOrders[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          elevation: 4,
+          child: ListTile(
             title: Text(order['name']!),
-            subtitle: Text('Order status: ${order['status']}'),
-            trailing: Icon(Icons.check_circle, color: Colors.green[600]),
-          );
-        },
-      ),
+            subtitle: Text('Ordered on ${order['date']}'),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                // Add delete logic
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
