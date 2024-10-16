f6d0ee0df6512e78e59096a42dd64132e36b3c71
Ishan-Hegde <110191499+Ishan-Hegde@users.noreply.github.com>
2024-10-16 21:10:38 +0530

    changed baseURL in config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -2,7 +2,8 @@
 
 class Config {
   // Replace with your Render app URL
-  static const String baseUrl = 'https://your-app-name.onrender.com/api/auth/';
+  static const String baseUrl =
+      'https://green-table-backend.onrender.com/api/auth/';
 
   static String getLoginUrl(bool isConsumerSelected) {
     return '${baseUrl}login'; // Assuming the endpoint for login is the same for both consumer and restaurant logins.
